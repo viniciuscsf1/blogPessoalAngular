046893fbb086396fdb3e3b080ef44051e2cfec58
unknown <vinicius130196@gmail.com>
2021-10-05 12:31:39 -0300

    Componente de edição e de deletar Tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,7 +19,22 @@ export class TemaService {
     return this.http.get<Tema[]>('https://viniblog.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://viniblog.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://viniblog.herokuapp.com/tema', tema, this.token)
-  } 
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://viniblog.herokuapp.com/tema', tema, this.token)
+  }
+  
+    //n precisa de observable no delete pq n preciso de um objeto, só do id dele.
+    //La no back end q o trabalho será feito e ele sera apagado
+  deleteTema(id:number){      
+    return this.http.delete(`https://viniblog.herokuapp.com/tema/${id}`, this.token)
+  }// aqui muda a forma de passar a rota pois la no backend ele pede parâmetro
 }
+
