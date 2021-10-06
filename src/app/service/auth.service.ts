a4ca95f5e3168450ae468eb993bfcaaaaa3fc198
unknown <vinicius130196@gmail.com>
2021-10-06 15:40:58 -0300

    Implementação do post e get de postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -23,6 +23,11 @@ export class AuthService {
     return this.http.post<User>('https://viniblog.herokuapp.com/usuarios/cadastrar',user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://viniblog.herokuapp.com/usuarios/${id}`)
+  }
+
+
   logado(){
     let ok: boolean = false
     if (environment.token != ""){
