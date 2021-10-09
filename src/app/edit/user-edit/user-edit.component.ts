71f497f2b7e46cf55112f99658752dee3f8c3a7e
unknown <vinicius130196@gmail.com>
2021-10-09 18:51:04 -0300

    Alertas- ainda falta corrigir

--- src/app/edit/user-edit/user-edit.component.ts
+++ src/app/edit/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/model/User';
+import { AlertasService } from 'src/app/service/alertas.service';
 import { AuthService } from 'src/app/service/auth.service';
 import { environment } from 'src/environments/environment.prod';
 
@@ -19,7 +20,8 @@ export class UserEditComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -44,11 +46,11 @@ export class UserEditComponent implements OnInit {
     this.user.tipo = this.tipoUsuario
   
     if (this.user.senha != this.confirmarSenha){
-      alert("as senhas estão incorretas")
+      this.alertas.showAlertDanger("As senhas estão incorretas")
     }else{
       this.authService.atualizar(this.user).subscribe((resp: User)=> {
       this.user = resp
-      alert("Usuário atualizado com sucesso, faça o login novamente.")
+      this.alertas.showAlertSuccess("Usuário atualizado com sucesso, faça o login novamente.")
       environment.token =''
       environment.nome =''
       environment.foto = ''
